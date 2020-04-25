f0c65260043a38228a791be56e3fd7289a137fb7
Lami Beach <oogunde0413@gmail.com>
2020-04-25 16:47:27 -0700

    last update push

--- public/server.js
+++ public/server.js
@@ -14,21 +14,45 @@ app.use( express.static('./form') );
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-    response.status(200).send('You\re not as stupid as you thought');
+    response.status(200).render('public/form/index.html');
 });
 
-app.get('/loveofmylife', (request, response) => {
-    response.status(200).send('Message in a bottle')
-});
 
 app.get('/person', (request, response) => {
     let data = {
         name: request.query.name,
         pets: ['dogs', 'cats', 'lizards', 'giraffe'],
     }
-
     response.status(200).render('pages/index.ejs', {person: data});
 });
+
+app.get('pages/searches/new.ejs', (request, response) => {
+    response.status(200).render('pages/searches/new.ejs');
+})
+
+app.post('/search', (request, response) => {
+    //google apis
+    let url = 'https://books.google.com/ebooks?id=buc0AAAAMAAJ&dq=holmes&as_brr=4&source=webstore_bookcard'
+    let queryObject = {
+        q: `${request.body.searchby}:${request.body.search}`
+    }
+    response.status(200).render('/searches/new')
+    
+});
+
+superagent.get(url)
+    .query(queryObject)
+    .then(results => {
+        let books = results.body.items.map(book => new Book(book));
+        response.status(200).render('searches/show.ejs',{books:books})
+    })
+
+    function Book(data){
+        this.title = data.volumeInfo.title;
+        this.amount = data.saleInfo.listPrice ? data.saleInfo.listPrice.amount: 'Unknown';
+    }
+
+
 
 app.use('*', (request, response) => {
     console.log(request);
