2793d2b4e01160b1de0c727d12a831dbe26f3551
Lami Beach <oogunde0413@gmail.com>
2020-04-26 14:48:19 -0700

    added files

--- server.js
+++ server.js
@@ -1,66 +1,77 @@
 'use strict';
 
-const PORT = process.env.PORT || 3000;
-
 require('dotenv').config();
 
 const express = require('express');
 const superagent = require('superagent');
-const pg = require('pg');
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-app.use( express.urlencoded({extended:true }));
-app.use( express.static('./form') );
+//brings in EJS
 app.set('view engine', 'ejs');
 
-app.get('/', (request, response) => {
-    response.status(200).render('public/form/index.html');
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('./public'));
+
+//If it finds the route
+app.get ('/', (request, response) => {
+  response.status(200).send('Hola Mundo');
 });
 
-
-app.get('/person', (request, response) => {
-    let data = {
-        name: request.query.name,
-        pets: ['dogs', 'cats', 'lizards', 'giraffe'],
-    }
-    response.status(200).render('pages/index.ejs', {person: data});
+//test route to check if index.ejs works
+app.get('/test', (request, response) => {
+  response.status(200).render('pages/index');
 });
 
-app.get('pages/searches/new.ejs', (request, response) => {
-    response.status(200).render('pages/searches/new.ejs');
-})
-
-app.post('/search', (request, response) => {
-    //google apis
-    let url = 'https://books.google.com/ebooks?id=buc0AAAAMAAJ&dq=holmes&as_brr=4&source=webstore_bookcard'
-    let queryObject = {
-        q: `${request.body.searchby}:${request.body.search}`
-    }
-    response.status(200).render('/searches/new')
-    
+//new search route
+app.get('/new', (request, response) => {
+  response.status(200).render('pages/searches/new')
 });
 
-superagent.get(url)
+//show route
+app.post('/searches', (request, response) => {
+  let url = 'https://www.googleapis.com/books/v1/volumes';
+  let queryObject = {
+    q: `${request.body.searchby}:${request.body.search}`,
+  };
+
+  superagent.get(url)
     .query(queryObject)
-    .then(results => {
+      .then(results => {
         let books = results.body.items.map(book => new Book(book));
-        response.status(200).render('searches/show.ejs',{books:books})
-    })
-
-    function Book(data){
-        this.title = data.volumeInfo.title;
-        this.amount = data.saleInfo.listPrice ? data.saleInfo.listPrice.amount: 'Unknown';
-    }
-
-
-
-app.use('*', (request, response) => {
-    console.log(request);
-    response.status(404).send(`Can't find ${request.pathname}`);
+        response.status(200).render('pages/searches/show', {books: books});
+      });
 });
 
+function Book(data) {
+  this.title = data.volumeInfo.title;
+  this.author = data.volumeInfo.authors;
+  this.image = data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : `https://i.imgur.com/J5LVHEL.jpg`;
+  this.description = data.volumeInfo.description;
+}
+
+
+// This will force an error
+app.get('/badthing', (request,response) => {
+  throw new Error('bad request???');
+});
+
+// 404 Handler
+app.use('*', (request, response) => {
+  console.log(request);
+  response.status(404).send(`Can't Find ${request.path}`);
+});
+
+// Error Handler
+app.use( (err,request,response,next) => {
+  console.error(err);
+  response.status(500).send(err.message);
+});
+
+// Startup
 function startServer() {
-app.listen(PORT, () => console.log(`Server up on ${PORT}`));
+  app.listen( PORT, () => console.log(`Server running on ${PORT}`));
 }
 
 startServer();
