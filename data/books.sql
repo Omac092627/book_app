3bfcde2b781db6e91ff72c6a9f3574eaa166174b
Lami Beach <oogunde0413@gmail.com>
2020-04-30 20:19:23 -0700

    homepage

--- data/books.sql
+++ data/books.sql
@@ -4,8 +4,7 @@ CREATE TABLE books (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   authors VARCHAR(100),
-  images TEXTIMAGE_ON,
   descriptions VARCHAR(500),
   isbn VARCHAR(13),
-  listPrice VARCHAR(20),
+  listPrice VARCHAR(20)
 )
